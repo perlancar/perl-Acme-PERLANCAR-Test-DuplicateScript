fd35d9614797b2a12bce5be6b90a9374c8f12c62
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-10 19:23:34 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/DuplicateScript.pm
@@ -0,0 +1,16 @@
+package Acme::PERLANCAR::Test::DuplicateScript;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Test duplicate script names in a distribution
+
+=head1 DESCRIPTION
+
+This distribution contains the following scripts:
+
+ bin/test-duplicate-script
+ script/test-duplicate-script
+
+which have the same base name. This distribution is for testing purposes only.
